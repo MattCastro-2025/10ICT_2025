945b8afe6c63363264cffd9cd917a8c06c00f605
MattCastro-2025 <mc22053@students.sacredheart.school.nz>
2025-07-28 00:50:33 +0000

    Update 6

--- website.py
+++ website.py
@@ -3,15 +3,11 @@ import requests
 from datetime import datetime
 
 
-@route('/')
-def index():
-    now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
-    return template('index', time=current_time)
-
-    name = 'Matt'
+@route('/<name>')
+def index(name):
     response = requests.get(f"https://api.agify.io/?name={name}")
     response = response.json()
     age = response['age']
     return template('index', name=name, age=age)
+
 run(host='0.0.0.0', port=4000, reloader=True, debug=True)
